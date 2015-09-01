643a97b0ae44d9786237a0f6d2c83b99f620128f
josboris27 <josboris27@gmail.com>
2015-09-02 00:38:28 +0300

    begin work

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+var webpack = require('gulp-webpack');
+var less = require('gulp-less');
+var concatCss = require('gulp-concat-css');
+var minifyCss = require('gulp-minify-css');
+var notify = require("gulp-notify");
+var concat = require('gulp-concat');
+var jsmin = require('gulp-jsmin');
+var rename = require("gulp-rename");
+var livereload = require('gulp-livereload');
+var cssnext = require("gulp-cssnext");
+
+livereload({ start: true });
+
+// ======== Html TASKS ============ //
+gulp.task('HTML', function () {
+    gulp.src('./src/html/*.html')
+        .pipe(gulp.dest('./dev/'))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(livereload())
+});
+
+// ======== Js TASKS ============ //
+gulp.task('JS', function() {
+    gulp.src('./src/scripts/app/app.js')
+        .pipe(webpack({
+            module: {
+                loaders: [
+                    {
+                        test: /\.js/,
+                        loader: 'babel-loader',
+                        exclude: /node_modules/,
+                        query: {
+                            optional: ['runtime'],
+                            stage: 0
+                        }
+                    },
+                    {
+                        test: /\.json/,
+                        loader: 'json-loader'
+                    }
+                ]
+            }
+        }))
+        .pipe(rename("app.js"))
+        .pipe(gulp.dest('./dev/scripts'))
+        //.pipe(rename({suffix: '.min'}))
+        //.pipe(jsmin())
+        //.pipe(gulp.dest('./dist/scripts'));
+    .pipe(livereload());
+});
+
+// ======== Css TASKS ============ //
+gulp.task('CSS', function () {
+    gulp.src('./src/styles/app.css')
+        //.pipe(less())
+        .pipe(cssnext({
+            compress: true
+        }))
+        .pipe(gulp.dest('./dev/styles'))
+        //.pipe(minifyCss())
+        //.pipe(rename({suffix: ".min"}))
+        //.pipe(gulp.dest('./dist/styles'))
+        .pipe(livereload());
+});
+
+// ======== Watch TASKS ============ //
+gulp.task('watch', function () {
+
+    gulp.watch('./src/html/**/*.html', ['HTML']);
+    gulp.watch('./src/styles/**/**/*.css', ['CSS']);
+    gulp.watch('./src/scripts/app/**/**/*.js', ['JS']);
+
+    livereload.listen();
+});
+
+gulp.task('default', ['watch']);
